d9d29aec544eda2ab7de07298bc0b785660e2f4b
Bicri <isaac.bicri@gmail.com>
2022-09-16 17:28:00 -0500

    services finished

--- javascript/app.js
+++ javascript/app.js
@@ -76,37 +76,92 @@
     }
 
 
-// //-- FIN CAROUSEL -----------------------------------------------
+//-- FIN CAROUSEL -----------------------------------------------
 
-//     let carouselContenedor = document.querySelector(".carousel__container");
-//     let anchoCard;
-//     let tamMax;
+//-- SECCION CARD CAROUSEL --------------------------------------
 
-//     function actualizarInfo() {
-//         anchoCard = document.querySelector(".card").offsetWidth + 16; //Se suma el ancho de la card + margin
-//         tamMax = (document.querySelectorAll(".card").length * anchoCard) - document.querySelector(".carousel__container").clientWidth;
-//     }
+function moverIzqCards(elem, unit) {
+    let recorrido = elem.scrollLeft; //Cuanto se ha movido el carousel
 
-//     document.querySelector(".carousel__boton--prev").addEventListener("click", () =>{
-//         actualizarInfo(); //En caso de modificar el ancho de la pantalla
-//         if (carouselContenedor.scrollLeft > 0) {
-//             moverIzqCards(carouselContenedor, -anchoCard ); //El negativo lo mueve a la derecha
-//         }
-//     });
+    //Mueve el carousel sumandole el ancho de la card
+    elem.scrollLeft = ((unit) + recorrido);
+}
 
-//     document.querySelector(".carousel__boton--next").addEventListener("click", ()=>{
-//         actualizarInfo(); //En caso de modificar el ancho de la pantalla
-//         if (carouselContenedor.scrollLeft < tamMax) {
-//             moverIzqCards(carouselContenedor, anchoCard /* x numCards a mover*/);
-//         }
-//     });
+//Carousel Facial
+    let carouselContenedor1 = document.querySelector(".faciales .cardCarousel__container");
+    let anchoCard1;
+    let tamMax1;
 
-//     function moverIzqCards(elem, unit) {
-//         let recorrido = elem.scrollLeft; //Cuanto se ha movido el carousel
+    function actualizarInfoFacial() {
+        anchoCard1 = document.querySelector(".faciales .card").offsetWidth + 16; //Se suma el ancho de la card + margin
+        tamMax1 = (document.querySelectorAll(".faciales .card").length * anchoCard1) - document.querySelector(".faciales .cardCarousel__container").clientWidth;
+    }
 
-//         //Mueve el carousel sumandole el ancho de la card
-//         elem.scrollLeft = ((unit) + recorrido);
-//     }
+    document.querySelector(".faciales .cardCarousel__boton--prev").addEventListener("click", () =>{
+        actualizarInfoFacial(); //En caso de modificar el ancho de la pantalla
+        if (carouselContenedor1.scrollLeft > 0) {
+            moverIzqCards(carouselContenedor1, -anchoCard1 ); //El negativo lo mueve a la derecha
+        }
+    });
+
+    document.querySelector(".faciales .cardCarousel__boton--next").addEventListener("click", ()=>{
+        actualizarInfoFacial(); //En caso de modificar el ancho de la pantalla
+        if (carouselContenedor1.scrollLeft < tamMax1) {
+            moverIzqCards(carouselContenedor1, anchoCard1 /* x numCards a mover*/);
+        }
+    });
+
+
+
+//Carousel Corporales
+    let carouselContenedor2 = document.querySelector(".corporales .cardCarousel__container");
+    let anchoCard2;
+    let tamMax2;
+
+    function actualizarInfoCorporal() {
+        anchoCard2 = document.querySelector(".corporales .card").offsetWidth + 16; //Se suma el ancho de la card + margin
+        tamMax2 = (document.querySelectorAll(".corporales .card").length * anchoCard2) - document.querySelector(".corporales .cardCarousel__container").clientWidth;
+    }
+
+    document.querySelector(".corporales .cardCarousel__boton--prev").addEventListener("click", () =>{
+        actualizarInfoCorporal(); //En caso de modificar el ancho de la pantalla
+        if (carouselContenedor2.scrollLeft > 0) {
+            moverIzqCards(carouselContenedor2, -anchoCard2 ); //El negativo lo mueve a la derecha
+        }
+    });
+
+    document.querySelector(".corporales .cardCarousel__boton--next").addEventListener("click", ()=>{
+        actualizarInfoCorporal(); //En caso de modificar el ancho de la pantalla
+        if (carouselContenedor2.scrollLeft < tamMax2) {
+            moverIzqCards(carouselContenedor2, anchoCard2 /* x numCards a mover*/);
+        }
+    });
+//Carousel quirurgicos
+    let carouselContenedor3 = document.querySelector(".quirurgicos .cardCarousel__container");
+    let anchoCard3;
+    let tamMax3;
+
+    function actualizarInfoQuirurgicos() {
+        anchoCard3 = document.querySelector(".quirurgicos .card").offsetWidth + 16; //Se suma el ancho de la card + margin
+        tamMax3 = (document.querySelectorAll(".quirurgicos .card").length * anchoCard3) - document.querySelector(".quirurgicos .cardCarousel__container").clientWidth;
+    }
+
+    document.querySelector(".quirurgicos .cardCarousel__boton--prev").addEventListener("click", () =>{
+        actualizarInfoQuirurgicos(); //En caso de modificar el ancho de la pantalla
+        if (carouselContenedor3.scrollLeft > 0) {
+            moverIzqCards(carouselContenedor3, -anchoCard3 ); //El negativo lo mueve a la derecha
+        }
+    });
+
+    document.querySelector(".quirurgicos .cardCarousel__boton--next").addEventListener("click", ()=>{
+        actualizarInfoQuirurgicos(); //En caso de modificar el ancho de la pantalla
+        if (carouselContenedor3.scrollLeft < tamMax3) {
+            moverIzqCards(carouselContenedor3, anchoCard3 /* x numCards a mover*/);
+        }
+    });
+
+    
+//--------------------------------------------------------------------------------------
 
 
 
